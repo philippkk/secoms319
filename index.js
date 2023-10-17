97065e83f69290ba77a25f2785b928c8814e13dc
Philip King <woahitssq@gmail.com>
2023-10-17 18:53:45 -0500

    one more time

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ let color = "";
 
 
 getData();
-setInterval(getData,1000);
+setInterval(getData,10000);
 
 function tempColor(temp){
     if(temp > 80){
@@ -59,7 +59,9 @@ function getHumidityData(){
 function setTempData(data){
     console.log(data);
     tempDataF.innerHTML = "<strong>"+data.data[0].temp_f + "°F</strong>";
+    tempDataF.style.padding = '15px';
     tempDataC.innerHTML = "<strong>"+data.data[0].temp_c + "°C</strong>";
+    tempDataC.style.padding = '15px';
     tempDate.innerHTML = "<strong>Date: " +data.data[0].date+"</strong>";
 
     setTempColor(tempDataF,data.data[0].temp_f);
@@ -71,6 +73,7 @@ function setTempColor(back,temp){
 function setHumdityData(data){
     console.log(data);
     humid.innerHTML = "<strong>"+data.data[0].humid +"%</strong>";
+    humid.style.padding = '15px';
     humidDate.innerHTML = "<strong>Date: " + data.data[0].date+"</strong>";
 
     setHumidColor(data.data[0].humid);

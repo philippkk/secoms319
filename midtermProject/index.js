43d540053ad92c2f6d1c21cd0acdd2a17bf4fe43
Philip King <woahitssq@gmail.com>
2023-10-17 17:09:52 -0500

    files for json and some website changes

--- midtermProject/index.js
+++ midtermProject/index.js
@@ -1,1 +1,20 @@
 console.log("hehe");
+
+let tempData = document.getElementById('tempData');
+
+let coolColor = "#a2b0ee";
+let midColor = "#47a15c";
+let hotColor = "#db4848";
+
+function setTempColor(temp){
+    if(temp > 75){
+        tempData.style.color = hotColor
+    }else if(temp > 45){
+        tempData.style.color = midColor
+    }else{
+        tempData.style.color = coolColor
+    }
+}
+
+
+

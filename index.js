1fd08d2b0a86f80500917094ada79ccee40762dc
Philip King <woahitssq@gmail.com>
2023-10-17 18:38:56 -0500

    added files

---
+++ index.js
@@ -0,0 +1,79 @@
+console.log("hehe");
+
+
+let tempDataF = document.getElementById('tempDataF');
+let tempDataC = document.getElementById('tempDataC');
+let tempDate = document.getElementById('tempDate');
+let tempBack = document.getElementById('tempback');
+
+let humid = document.getElementById('humid');
+let humidDate = document.getElementById('humidDate');
+
+let coolR = 162;
+let coolG = 176;
+let coolB = 238;
+let hotR = 230;
+let hotG = 106;
+let hotB = 106;
+let color = "";
+
+
+getData();
+setInterval(getData,600000);
+
+function tempColor(temp){
+    if(temp > 80){
+        color = "rgb(230, 106, 106)";
+    }
+    if(temp < 30){
+        color = "rgb(162, 176, 238)";
+    }
+    var colorR = coolR + ((temp-30)/50)*(hotR-coolR);
+    var colorG = coolG + ((temp-30)/50)*(hotG-coolG);
+    var colorB = coolB + ((temp-30)/50)*(hotB-coolB);
+    color = "rgb("+colorR+","+colorG+","+colorB+")";
+    return color;
+}
+function humidityColor(hunidity){
+    var colorR = coolR + ((hunidity)/100)*(hotR-coolR);
+    var colorG = coolG + ((hunidity)/100)*(hotG-coolG);
+    var colorB = coolB + ((hunidity)/100)*(hotB-coolB);
+    color = "rgb("+colorR+","+colorG+","+colorB+")";
+    return color;
+}
+function getData(){
+    getTempData();
+    getHumidityData();
+}
+function getTempData(){
+    fetch('./data.json')
+    .then(response => response.json())
+    .then(data => setTempData(data));
+}
+function getHumidityData(){
+    fetch('./data.json')
+    .then(response => response.json())
+    .then(data => setHumdityData(data));
+}
+function setTempData(data){
+    tempDataF.innerHTML = "<strong>"+data.data[0].temp_f + "°F</strong>";
+    tempDataC.innerHTML = "<strong>"+data.data[0].temp_c + "°C</strong>";
+    tempDate.innerHTML = "<strong>Date: " +data.data[0].date+"</strong>";
+
+    setTempColor(tempDataF,data.data[0].temp_f);
+    setTempColor(tempDataC,data.data[0].temp_f);
+}
+function setTempColor(back,temp){
+    back.style.backgroundColor = tempColor(temp);
+}
+function setHumdityData(data){
+    humid.innerHTML = "<strong>"+data.data[0].humid +"%</strong>";
+    humidDate.innerHTML = "<strong>Date: " + data.data[0].date+"</strong>";
+
+    setHumidColor(data.data[0].humid);
+}
+function setHumidColor(humidity){
+    humid.style.backgroundColor = humidityColor(humidity);
+}
+
+

5525fa22e32ea4dddf201569ca8c4c19c11216b1
Philip King <woahitssq@gmail.com>
2023-10-17 21:14:45 -0500

    finished site

--- midtermProject/index.js
+++ midtermProject/index.js
@@ -5,6 +5,8 @@ let tempDataF = document.getElementById('tempDataF');
 let tempDataC = document.getElementById('tempDataC');
 let tempDate = document.getElementById('tempDate');
 let tempBack = document.getElementById('tempback');
+let tempImage = document.getElementById('tempPic');
+let humidPic = document.getElementById('humidPic');
 
 let humid = document.getElementById('humid');
 let humidDate = document.getElementById('humidDate');
@@ -19,7 +21,7 @@ let color = "";
 
 
 getData();
-setInterval(getData,10000);
+setInterval(getData,100000);
 
 function tempColor(temp){
     if(temp > 80){
@@ -66,10 +68,62 @@ function setTempData(data){
 
     setTempColor(tempDataF,data.data[0].temp_f);
     setTempColor(tempDataC,data.data[0].temp_f);
+    setTempPic(data.data[0].temp_f);
+    setTempAvg(data);
 }
 function setTempColor(back,temp){
     back.style.backgroundColor = tempColor(temp);
 }
+function setTempPic(temp){
+    if(temp >= 80){
+        var num = Math.random() ;
+        console.log(num);
+        if(num > .5){
+            tempImage.src = "./images/snowmanBoiling.jpg";
+        }else{
+            tempImage.src = "./images/boilingtwo.jpg";
+        }
+    }else if(temp >= 60){
+        tempImage.src = "./images/snowmanSpring.jpg";
+    }else if(temp >= 40){
+        tempImage.src = "./images/cnowmanMelting.jpg";
+    }else{
+        tempImage.src = "./images/snowmanCold.jpg";
+    }
+}
+function setTempAvg(data){
+    var hour = 0;
+    var day = 0;
+    var ytd = 0;
+    for(i = 0;i < data.data.length;i++){
+        if(i < 6){
+            hour += data.data[i].temp_f;
+        }
+        if(i < 144){
+            day += data.data[i].temp_f;
+        }
+        ytd +=data.data[i].temp_f
+    }
+    if(data.data.length< 6){
+        hour/=data.data.length;
+    }else{
+        hour/=6;
+    }
+    if(data.data.length<144){
+        day/=data.data.length;
+    }else{
+        day/=144;
+    }
+    document.getElementById('t1hour').innerHTML = "<strong>1 hour average: " + hour.toFixed(2) +"F°</strong>";
+    document.getElementById('t1hour').style.backgroundColor = tempColor(hour);
+    document.getElementById('t24hour').innerHTML= "<strong>1 day average: " + day.toFixed(2) +"F°</strong>";
+    document.getElementById('t24hour').style.backgroundColor = tempColor(day);
+    document.getElementById('tytd').innerHTML= "<strong>ytd average: " + (ytd/data.data.length).toFixed(2) +"F°</strong>";
+    document.getElementById('tytd').style.backgroundColor = tempColor((ytd/data.data.length).toFixed(2));
+
+}
+
+
 function setHumdityData(data){
     console.log(data);
     humid.innerHTML = "<strong>"+data.data[0].humid +"%</strong>";
@@ -77,9 +131,49 @@ function setHumdityData(data){
     humidDate.innerHTML = "<strong>Date: " + data.data[0].date+"</strong>";
 
     setHumidColor(data.data[0].humid);
+    setHumidPic(data.data[0].humid);
+    setHumidityAvg(data);
 }
 function setHumidColor(humidity){
     humid.style.backgroundColor = humidityColor(humidity);
+}
+function setHumidPic(humidity){
+    if(humidity <= 50){
+        humidPic.src = "./images/dryman.jpg";
+    }else{
+        humidPic.src = "./images/highhumid.jpg";
+
+    }
+}
+function setHumidityAvg(data){
+        var hour = 0;
+        var day = 0;
+        var ytd = 0;
+        for(i = 0;i < data.data.length;i++){
+            if(i < 6){
+                hour += data.data[i].humid;
+            }
+            if(i < 144){
+                day += data.data[i].humid;
+            }
+            ytd +=data.data[i].humid
+        }
+        if(data.data.length< 6){
+            hour/=data.data.length;
+        }else{
+            hour/=6;
+        }
+        if(data.data.length<144){
+            day/=data.data.length;
+        }else{
+            day/=144;
+        }
+        document.getElementById('h1').innerHTML = "<strong>1 hour average: " + hour.toFixed(2) +"%</strong>";
+        document.getElementById('h1').style.backgroundColor = tempColor(hour);
+        document.getElementById('h24').innerHTML= "<strong>1 day average: " + day.toFixed(2) +"%</strong>";
+        document.getElementById('h24').style.backgroundColor = tempColor(day);
+        document.getElementById('hytd').innerHTML= "<strong>ytd average: " + (ytd/data.data.length).toFixed(2) +"%</strong>";
+        document.getElementById('hytd').style.backgroundColor = tempColor((ytd/data.data.length).toFixed(2));
 }
 
 

4f9a8b404d4bb1e16c5399aececd1f399488a07c
Philip King <woahitssq@gmail.com>
2023-10-30 13:42:12 -0500

    ok i got it this time somehow

---
+++ Assignment2/assignement2/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 mt-4 ">
+  Button
+  </button>
+);
+

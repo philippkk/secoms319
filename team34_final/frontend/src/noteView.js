6ee43524ea37561420c2b634bcfa14f138dc85f5
Cbolt17 <cboltjes@iastate.edu>
2023-11-29 21:39:36 -0600

    Added file functionality and viewing in project view
    What the title says.

--- team34_final/frontend/src/noteView.js
+++ team34_final/frontend/src/noteView.js
@@ -1,49 +1,75 @@
-import React from "react";
+import React, { useState} from "react";
 
-const NoteView = ({notes, note, setNote}) => {
-  const tags = <Tags tags={note.tags} />;
+const NoteView = ({ notes, note, changeView }) => {
+  const [currentID, setCurrentID] = useState(note.noteID);
+  const [title, setTitle] = useState(note.title);
+  const [content, setContent] = useState(note.content);
+  const [tags, setTags] = useState(note.tags);
+
+  // Used to save an updated note.
+  function save() {
+    note.content = content;
+    note.title = title;
+    note.tags = tags;
+    // Implement note saving here ///////////////////////////////////////////////////////
+  }
+  
+  if(note.noteID !== currentID) {
+    setCurrentID(note.noteID);
+    setTitle(note.title);
+    setContent(note.content);
+    setTags(note.tags);
+  }
+
+  const tagDiv = <Tags tags={tags} />;
   if (note.type === "text") {
     return (
       <div>
         <div>
-          <h1>{note.title}</h1>
+          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}>
+          </input>
         </div>
         <div>
-          <p>{note.content}</p>
+          <input type="text" value={content} onChange={e => setContent(e.target.value)}>
+          </input>
         </div>
-        <div>{tags}</div>
+        <div>Tags: {tagDiv}</div>
+        <button onClick={() => save()}>Save</button>
       </div>
     );
   }
 
-  const noteTabs = note.content.map((noteID) => (
-    <Note key={noteID} note={notes.find((n) => n.noteID === noteID)} setNote={setNote} />
-  ));
-
+  const noteTabs = note.content.map((noteID) => 
+    <Note
+      key={noteID}
+      note={notes.find((n) => n.noteID === noteID)}
+      changeView={changeView}
+    />
+  );
 
   return (
     <div>
       <div>
-        <h1>{note.title}</h1>
+          <input type="text" value={title} onChange={e => setTitle(e.target.value)}>
+          </input>
       </div>
-      <div>
-        <p>{note.text}</p>
-      </div>
-      <div>{tags}</div>
+      <div>{noteTabs}</div>
+      <div>Tags: {tagDiv}</div>
+      <button onClick={() => save()}>Save</button>
     </div>
   );
 };
 
 const Tags = ({ tags }) => {
-  let string = "";
-  for (var i = 0; i < tags.length; i++)
-    string += tags[i] + ", ";
-  let list = string.slice(0, -2);
-  return <div>{list}</div>;
+  return <div>{tags.map((tag) => <Tag name={tag} />)}</div>;
 };
 
-const Note = ({note, setNote}) => {
-    return (<button onClick={setNote(note.noteID)}>{note.title}</button>);
+const Tag = ({name}) => {
+  return <button>{name}</button>
 }
+
+const Note = ({ note, changeView }) => {
+  return <button onDoubleClick={() => changeView(note.noteID)}>{note.title}</button>;
+};
 
 export default NoteView;
